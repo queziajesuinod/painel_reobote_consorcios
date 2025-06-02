8fd1162101b792c596238ef64f484457b3f05931
macbookair <queziajesuino@gmail.com>
2025-06-02 08:12:58 -0400

    configuracoes

--- app.py
+++ app.py
@@ -172,4 +172,5 @@ def deletar_propaganda(id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=False)
+
