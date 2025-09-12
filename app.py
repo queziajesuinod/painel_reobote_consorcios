ff1698d45f6965eb0b1d01752214881bf4f8bdc4
Quézia Jesuino <quezitabjq@gmail.com>
2025-09-12 02:53:07 -0400

    retirar debug

--- app.py
+++ app.py
@@ -375,4 +375,4 @@ def toggle_campanha(id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=8182, debug=True)
+    app.run(host='0.0.0.0', port=8182, debug=False)
