ee862cf21aa21ea3a2473582af471cbd5b8b20c9
Quézia Jesuino <quezitabjq@gmail.com>
2025-11-27 04:09:18 -0400

    .

--- app.py
+++ app.py
@@ -265,8 +265,8 @@ def obter_dashboard_tarefas_dados():
     due_fim = datetime.combine(fim_mes + timedelta(days=1), time.min).strftime('%Y-%m-%dT%H:%M:%SZ')
     params_tarefas = {
         'per_page': 100,
-        'finishedAtGt': due_inicio,
-        'finishedAtLt': due_fim
+        'finishedDateGt': due_inicio,
+        'finishedDateLt': due_fim
     }
     tarefas = fetch_tasks(params_tarefas)
 
